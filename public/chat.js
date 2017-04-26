b4c0b92f9f810f7ae39caf65849098e61a14d28f
Michael Graf <michigraf2@gmail.com>
2017-04-26 15:04:07 +0200

    random names now given on the server + no duplicates

--- public/chat.js
+++ public/chat.js
@@ -3,7 +3,7 @@ const socket = io.connect('http://localhost:1337');
 
 socket.on('connect', ()=>{
 	$(".chatlog").append("<li>Connected</li>");
-	let nickname = prompt("Enter Nickname:", randomNickname());
+	let nickname = prompt("Enter Nickname:");
 	socket.emit("join", nickname);
 });
 
@@ -32,50 +32,3 @@ function sendMessage(){
 	$('#newMsg').val("");
 }
 
-function randomNickname(){
-	const NAMES = [
-		'Beat PunchBeef',
-		'Big, Brave Brick of Meat',
-		'Big McLargeHuge',
-		'Blast HardCheese',
-		'Blast ThickNeck',
-		'Bob Johnson',
-		'Bold BigFlank',
-		'Bolt VanderHuge',
-		'Brick HardMeat',
-		'Buck PlankChest',
-		'Buff DrinkLots',
-		'Buff HardBack',
-		'Butch DeadLift',
-		'Crud BoneMeal',
-		'Crunch ButtSteak',
-		'Dirk HardPec',
-		'Fist RockBone',
-		'Flint IronStag',
-		'Fridge LargeMeat',
-		'Gristle McThornBody',
-		'Hack BlowFist',
-		'Lump BeefBroth',
-		'Punch RockGroin',
-		'Punch Side-Iron',
-		'Punt SpeedChunk',
-		'Reef BlastBody',
-		'Roll Fizzlebeef',
-		'Rip SteakFace',
-		'Slab BulkHead',
-		'Slab SquatThrust',
-		'Slate Fistcrunch',
-		'Slate SlabRock',
-		'Smash LampJaw',
-		'Smoke ManMuscle',
-		'Splint ChestHair',
-		'Stump BeefKnob',
-		'Stump Chunkman',
-		'Thick McRunFast',
-		'Touch RustRod',
-		'Trunk SlamChest',
-		'Whip SlagCheek'
-	];
-	 let name = NAMES[Math.floor(Math.random() * NAMES.length)];
-	return name;
-}
--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.get('/', (req, res)=>{
 io.on('connection', (client)=>{
 
 	client.on("join",(name)=>{
-		client.nickname = name;
+		
+		client.nickname = name || randomNickname();
 		const time = postingTime();
 		client.broadcast.emit("message",'['+ time + ']: ' + client.nickname + ' has joined the chat');
 		client.broadcast.emit('userJoined', client.nickname);
@@ -65,4 +66,57 @@ function postingTime(){
 	let time = new Date();
 	time = time.toTimeString();
 	return time.match(/\d+:\d+:\d+/)[0];
+}
+
+function randomNickname(){
+	const NAMES = [
+		'Beat PunchBeef',
+		'Big, Brave Brick of Meat',
+		'Big McLargeHuge',
+		'Blast HardCheese',
+		'Blast ThickNeck',
+		'Bob Johnson',
+		'Bold BigFlank',
+		'Bolt VanderHuge',
+		'Brick HardMeat',
+		'Buck PlankChest',
+		'Buff DrinkLots',
+		'Buff HardBack',
+		'Butch DeadLift',
+		'Crud BoneMeal',
+		'Crunch ButtSteak',
+		'Dirk HardPec',
+		'Fist RockBone',
+		'Flint IronStag',
+		'Fridge LargeMeat',
+		'Gristle McThornBody',
+		'Hack BlowFist',
+		'Lump BeefBroth',
+		'Punch RockGroin',
+		'Punch Side-Iron',
+		'Punt SpeedChunk',
+		'Reef BlastBody',
+		'Roll Fizzlebeef',
+		'Rip SteakFace',
+		'Slab BulkHead',
+		'Slab SquatThrust',
+		'Slate Fistcrunch',
+		'Slate SlabRock',
+		'Smash LampJaw',
+		'Smoke ManMuscle',
+		'Splint ChestHair',
+		'Stump BeefKnob',
+		'Stump Chunkman',
+		'Thick McRunFast',
+		'Touch RustRod',
+		'Trunk SlamChest',
+		'Whip SlagCheek'
+	];
+	let name;
+
+	do
+	name = NAMES[Math.floor(Math.random() * NAMES.length)];
+	while(connectedUsers.length < NAMES.length && connectedUsers.indexOf(name) == -1);
+	
+	return name;
 }
