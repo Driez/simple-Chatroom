70f47366651b74e31fe8b9a09410021abbe7cb26
Michael Graf <michigraf2@gmail.com>
2017-05-08 18:56:40 +0200

    name check and badlanguage filter fix

--- chatfilters/filters.js
+++ chatfilters/filters.js
@@ -1,9 +1,22 @@
-const BADWORDS = ['anal', 'anus', 'arse', 'ass', 'ballsack', 'balls', 'bastard', 'bitch', 'biatch', 'bloody', 'blowjob', 'blow job', 'bollock', 'bollok', 'boner', 'boob', 'bugger', 'bum', 'butt', 'buttplug', 'clitoris', 'cock', 'coon', 'crap', 'cunt', 'damn', 'dick', 'dildo', 'dyke', 'fag', 'feck', 'fellate', 'fellatio', 'felching', 'fuck', 'f u c k', 'fudgepacker', 'fudge packer', 'flange', 'Goddamn', 'God damn', 'hell', 'homo', 'jerk', 'jizz', 'knobend', 'knob end', 'labia', 'lmao', 'lmfao', 'muff', 'nigger', 'nigga', 'omg', 'penis', 'piss', 'poop', 'prick', 'pube', 'pussy', 'queer', 'scrotum', 'sex', 'shit', 's hit', 'sh1t', 'slut', 'smegma', 'spunk', 'tit', 'tosser', 'turd', 'twat', 'vagina', 'wank', 'whore', 'wtf'];
+const BADWORDS = ['anal', 'anus', 'arse', 'ass', 'ballsack', 'balls', 'bastard', 'bitch', 'biatch', 'bloody', 'blowjob',
+'blow job', 'bollock', 'bollok', 'boner', 'boob', 'bugger', 'bum', 'butt', 'buttplug', 'clitoris', 'cock', 'coon', 'crap',
+'cunt', 'damn', 'dick', 'dildo', 'dyke', 'fag', 'feck', 'fellate', 'fellatio', 'felching', 'fuck', 'f u c k', 'fudgepacker',
+'fudge packer', 'flange', 'Goddamn', 'God damn', 'hell', 'homo', 'jerk', 'jizz', 'knobend', 'knob end', 'labia', 'muff', 'nigger',
+'nigga', 'omg', 'penis', 'piss', 'poop', 'prick', 'pube', 'pussy', 'queer', 'scrotum', 'sex', 'shit', 's hit', 'sh1t', 'slut', 'smegma',
+'spunk', 'tit', 'tosser', 'turd', 'twat', 'vagina', 'wank', 'whore', 'wtf'];
 
 
 
-module.exports.badLanguage = function (msg){
-	for(let i = 0; i < BADWORDS.length;)
-	if(msg.match(BADWORDS[i])) return false;
-	return true;
+ let badLanguage = function (msg){
+	const regExp = new RegExp('/' + BADWORDS.join('|') + '/');
+	return (!!msg.match(regExp)) ? false : true;
 };
+
+let username = function (name){
+	if(badLanguage(name)){
+		return name.match(/admin | superuser | mikemaster | driez /i) ? false : true;
+	}
+	return false;
+};
+
+module.exports = {username, badLanguage};
